772198b089457dca839b762d1d395448a922da7b
David García <doggcs0099@gmail.com>
2020-11-14 10:34:37 -0400

    InicioDesdeCero

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,26 +1,10 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase';
-import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
-
-  //Metodo para hacer login con Google
-
-  loginWithGoogle(): Promise<void>{
-    return this.authLogin(new auth.GoogleAuthProvider())
-    .then((response)=>{
-      if (response){
-        localStorage.setItem('user', JSON.stringify(response));
-      }
-    })
-    .catch((err) => console.log(err));
-  }
-
+  constructor() { }
   
 }
