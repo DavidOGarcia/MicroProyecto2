177dd906cc14d712bb5cb8ff741f53764b510940
David García <doggcs0099@gmail.com>
2020-11-14 10:31:38 -0400

    Inicio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA57uqrMQsy-bRT-r7NRkZ8sjTlI-i60sM",
+    authDomain: "micro-proyecto2-148f6.firebaseapp.com",
+    databaseURL: "https://micro-proyecto2-148f6.firebaseio.com",
+    projectId: "micro-proyecto2-148f6",
+    storageBucket: "micro-proyecto2-148f6.appspot.com",
+    messagingSenderId: "298348595052",
+    appId: "1:298348595052:web:2c80f48f8caea32a845549",
+    measurementId: "G-2D1Q7BBTQB"
+  }
 };
