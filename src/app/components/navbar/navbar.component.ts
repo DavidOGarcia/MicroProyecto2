448c07d7f5e0af48318aa6fe15cd31d0adaeb598
David García <doggcs0099@gmail.com>
2020-11-15 02:38:37 -0400

    Final

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'firebase';
+import { AuthService } from 'src/app/services/auth.service';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  isAuthenticated = false;
+  user:User = null;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser(): void{
+    this.authService.getCurrentUser().subscribe(response=>{
+      if(response){
+        this.isAuthenticated = true;
+        this.user =  response;
+        return;
+      }
+
+      this.isAuthenticated = false;
+      this.user = null;
+    });
+  }
+
+  logout(): void{
+    this.authService.logout().then(()=>{
+      this.router.navigate(['/']);
+    })
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase';
+import { auth, User } from 'firebase';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,23 @@ export class AuthService {
 
   loginWithGoogle(): Promise<void>{
     return this.authLogin(new auth.GoogleAuthProvider()).then((response)=>{
-      console.log(response.user.displayName, response.user.email);
+      localStorage.setItem('user', JSON.stringify(response.user))
     });
+  }
+
+  getCurrentUser(): Observable<User>{
+    return this.afAuth.authState;
+  }
+
+  isAuthenticated(): boolean{
+    const user: User = JSON.parse(localStorage.getItem('user'))?? null;
+    return user !==null;
+  }
+
+  logout(){
+    return this.afAuth.signOut().then(()=>{
+      localStorage.removeItem('user');
+    }).catch(error=>{});
   }
 
   private authLogin(provider: auth.AuthProvider): Promise<auth.UserCredential>{
